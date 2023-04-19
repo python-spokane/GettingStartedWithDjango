6e6f47edbd39235f11ab269653f054c3330e5509
joeriddles <joeriddles10@gmail.com>
2023-04-18 18:18:15 -0700

    Kill all the code

--- src/blog/urls.py
+++
@@ -1,8 +0,0 @@
-app_name = "blog"
-
-from django.urls import path
-from blog import views
-
-urlpatterns = [
-    path("", views.view_posts, name="view_posts"),
-]
--- src/blog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from blog import models
-
-
-def view_posts(request):
-    posts = models.Post.objects.all()
-    context = {"posts": posts}
-    return render(request, "blog/post_list.html", context)
